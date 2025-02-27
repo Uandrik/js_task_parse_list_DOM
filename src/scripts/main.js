76bd13b90413aad0db8f766ead48fc3d12d88aeb
Uandrik <uandriy4141@gmail.com>
2025-02-27 21:22:20 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+function sortList() {
+  const list = document.querySelector('ul');
+  const items = Array.from(list.querySelectorAll('li'));
+
+  items.sort((a, b) => {
+    return (
+      Number(b.getAttribute('data-salary').slice(1).replaceAll(',', '')) -
+      Number(a.getAttribute('data-salary').slice(1).replaceAll(',', ''))
+    );
+  });
+
+  list.innerHTML = '';
+  items.forEach((item) => list.appendChild(item));
+}
+
+sortList();
