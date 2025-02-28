5cce29caefdb4489838511b3e4c0a499cec51800
Uandrik <uandriy4141@gmail.com>
2025-02-28 06:08:19 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,9 @@
 'use strict';
 
-function sortList() {
-  const list = document.querySelector('ul');
-  const items = Array.from(list.querySelectorAll('li'));
+const list = document.querySelector('ul');
+const items = Array.from(list.querySelectorAll('li'));
 
+function sortList() {
   items.sort((a, b) => {
     return (
       Number(b.getAttribute('data-salary').slice(1).replaceAll(',', '')) -
@@ -15,4 +15,14 @@ function sortList() {
   items.forEach((item) => list.appendChild(item));
 }
 
+function getEmployees() {
+  return items.map((item) => ({
+    name: item.innerText,
+    position: item.getAttribute('data-position'),
+    salary: item.getAttribute('data-salary'),
+    age: item.getAttribute('data-age'),
+  }));
+}
+
 sortList();
+getEmployees();
